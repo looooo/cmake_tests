80db6c23c8560aede639d464aa7cef54d1d59fd5
looooo <sppedflyer@gmail.com>
2019-03-05 22:07:29 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+cmake -G "Ninja" \
+      -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
+      ${RECIPE_DIR}
+
+ninja -v
